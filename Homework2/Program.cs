aebd24dbc5e2fbb80f34dfc8cb20a5dc7cbd1e93
Nastja <coj-nastja@mail.ru>
2022-08-25 00:22:49 +0300

    Homework 2

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+
+int CutNumber(){
+    int num = new Random().Next(100,1000);
+    Console.WriteLine("Number is - " + num);
+    int dec = num / 100;
+    int ed = num % 10;
+    int result = dec*10 + ed;
+    return result;
+}

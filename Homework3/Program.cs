12650e5bc3cf14f9eaeea8b5c81ba538b9e62bc1
Nastja <coj-nastja@mail.ru>
2022-09-01 17:04:47 +0300

    Homework 3

---
+++ Homework3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+// void Number(){
+
+//     Console.Write("Введите пятизначное число: ");
+//    string num = Console.ReadLine();
+//     Console.WriteLine("Number is - ");
+//     if (num.Substring(0,1) == num.Substring(4,1) || num.Substring(1,1) == num.Substring(3,1) ){
+//         Console.WriteLine("Да Палиндром "+ num);
+//     }
+//     else{   
+//         Console.WriteLine("Не Палиндром "+ num);
+//     } 
+// };
+// Number();
+
+// //Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// void Square(int n){
+//     int curr =1;
+//     for (curr=1;curr<=n;curr++)
+//             Console.WriteLine(curr*curr*curr);
+// }
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num<= 0) Console.Write("Некорректный ввод");
+// else Square(num);
+
+
--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,1 +1,10 @@
-﻿// 
+﻿// напишите программу которая на вход примет число и выдаст ее квадрат
+int num;
+
+Console.Write("Введите число: ");
+
+num = Convert.ToInt32(Console.ReadLine());
+int square = num*num;
+
+Console.WriteLine("квадрат введенного числа: "+ square);
+

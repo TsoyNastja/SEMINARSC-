2680715481c6c67231a24923a53d8d84640c31e2
Nastja <coj-nastja@mail.ru>
2022-09-01 23:00:23 +0300

    Homework 3

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -41,3 +41,41 @@
 // else Square(num);
 
 
+// Задача 21
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+// void average(int x1,int y1,int z1,int x2,int y2,int z2){
+//     // double s = Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2);
+
+//     double s = Math.Sqrt(
+//         Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2) 
+//         );
+//                 Console.WriteLine($"Расстояние между двумя точками в пространстве {Math.Round(s, 2) }");
+//                 Console.ReadKey();
+            
+            
+// }
+
+// int inputCoordinate(string name){
+//     Console.Write($"Введите координаты {name} : ");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int x1 = inputCoordinate("x1");
+// int y1 = inputCoordinate("y1");
+// int z1 = inputCoordinate("z1");
+// int x2 = inputCoordinate("x2");
+// int y2 = inputCoordinate("y2");
+// int z2 = inputCoordinate("z2");
+
+                
+// average( x1, y1, z1,x2,y2,z2);
+
+
+

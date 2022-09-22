b44f5345f7ba5ec111e20f1db177586999471617
Nastja <coj-nastja@mail.ru>
2022-09-22 21:14:23 +0300

    Homework 8

---
+++ Homework9/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+// Console.Write("Введи N: ");
+// int N = Convert.ToInt16(Console.ReadLine());
+// Console.Write("Введи M: ");
+// int M = Convert.ToInt16(Console.ReadLine());
+// Console.WriteLine($"N={N}, M={M}");
+// if (M>N)
+// for (int i = N; i <= M; i++)
+//     Console.Write($" {i}");
+// else
+//     for (int i = M; i <= N; i++)
+//         Console.Write($" {i}");
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int n, m, sum=0;
+// Console.Write("Введи значение от: ");
+// n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введи значение до: ");
+// m = Convert.ToInt32(Console.ReadLine());
+// for (int i = n; i <= m; i++)
+//     sum += i;
+// Console.WriteLine(sum);
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 29
+
+int AkkermanFunc(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return AkkermanFunc(m - 1, 1);
+			}
+			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+		}
+		
+		Console.WriteLine(AkkermanFunc(3,2));
+	
